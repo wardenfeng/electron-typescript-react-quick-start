3be95b8907f35e4e4ad4ee748a5eb46980301733
feng <908087098@qq.com>
2016-10-26 17:32:59 +0800

    显示 feng3d

--- src/app/hello-world.tsx
+++ src/app/hello-world.tsx
@@ -7,7 +7,10 @@ export default class HelloWorld extends React.Component<{}, {}> {
         return (
             <div className="hello-world">
                 Hello, world!
-            </div>
+                    <canvas id="glcanvas" width="640" height="480">
+                    Your browser doesn't appear to support the HTML5 <code>&lt;canvas&gt;</code> element.
+                    </canvas>
+            </div >
         );
     }
 }
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,8 @@
 import Greeting from './greeting';
+import ColorMaterialTest from './feng3d/ColorMaterialTest';
 
 const greeting = new Greeting();
 
 greeting.hello();
 
+new ColorMaterialTest();
