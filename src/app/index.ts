ecd705ad949f2fe39f29fade1f9bce959798a349
feng <908087098@qq.com>
2016-10-26 20:39:14 +0800

    配置 material-ui 环境

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,10 @@
 import Greeting from './greeting';
 import ColorMaterialTest from './feng3d/ColorMaterialTest';
 
+import * as injectTapEventPlugin from "react-tap-event-plugin";
+
+injectTapEventPlugin();
+
 const greeting = new Greeting();
 
 greeting.hello();
